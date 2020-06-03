a2ec4917f754721f86d46e5d29a3aa632df5fedc
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-06-03 15:55:02 +0100

    remove unused imports

--- python/test/unit/fem/test_fem_pipeline.py
+++ python/test/unit/fem/test_fem_pipeline.py
@@ -17,10 +17,9 @@ from dolfinx import (DirichletBC, Function, FunctionSpace, fem, geometry, cpp,
                      Mesh, UnitCubeMesh, UnitSquareMesh)
 from dolfinx.fem import (apply_lifting, assemble_matrix, assemble_scalar,
                          assemble_vector, locate_dofs_topological, set_bc)
-from mpi4py import MPI
 from dolfinx.cpp.mesh import CellType
 from dolfinx.io import XDMFFile
-from dolfinx_utils.test.skips import skip_if_complex, skip_in_parallel
+from dolfinx_utils.test.skips import skip_in_parallel
 from ufl import (SpatialCoordinate, TestFunction, TrialFunction, div, dx, grad,
                  inner, ds, dS, avg, jump, FacetNormal, CellDiameter)
 
